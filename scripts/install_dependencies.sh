882a1b1e7fb2f4ec68e31f69fab9d153ec7cb2a2
David Hormigo <dhorram948@g.educaand.es>
2023-02-04 23:21:08 +0100

    cambios scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,4 +5,4 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 nvm install --lts
 ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/local/bin/node"
 ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/npm" "/usr/local/bin/npm"
-mkdir /home/ubuntu/logs
+mkdir /home/ubuntu/app/release/logs
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/app/release
-npm run start
+npm start
